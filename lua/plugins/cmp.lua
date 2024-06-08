6c9e4bca19e752d26e657572dc52e5dfa183d36e
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2024-06-09 00:55:52 +0200

    enable autosnippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,6 @@
 return { -- Autocompletion
   'hrsh7th/nvim-cmp',
-  event = 'InsertEnter',
+  -- event = 'InsertEnter',
   dependencies = {
     -- Snippet Engine & its associated nvim-cmp source
     {
@@ -16,9 +16,6 @@ return { -- Autocompletion
         return 'make install_jsregexp'
       end)(),
       dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
         -- {
         --   'rafamadriz/friendly-snippets',
         --   config = function()
@@ -62,7 +59,9 @@ return { -- Autocompletion
     -- See `:help cmp`
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
-    luasnip.config.setup {}
+    luasnip.config.setup {
+      enable_autosnippets = true,
+    }
 
     cmp.setup {
       snippet = {
@@ -113,9 +112,9 @@ return { -- Autocompletion
       sources = {
 
         { name = 'nvim_lsp' },
-        { name = 'vim-dadbod-completion' },
         { name = 'luasnip' },
         { name = 'path' },
+        { name = 'vim-dadbod-completion' },
       },
     }
   end,
