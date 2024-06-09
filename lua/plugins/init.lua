7ac8ea8b6b3fd16e22eb63633655983a5bfae2ca
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2024-06-09 16:40:10 +0200

    harpoon

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,6 @@ return {
   { 'catppuccin/nvim', event = 'VeryLazy' },
 
   -- Improvements
-  { 'ThePrimeagen/harpoon', event = 'VeryLazy' },
   { 'lewis6991/impatient.nvim' },
   { 'ggandor/lightspeed.nvim', event = 'InsertEnter' },
   { 'max397574/better-escape.nvim', event = 'InsertEnter' },
