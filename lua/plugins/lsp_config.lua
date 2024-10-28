fbdc4224b805712de421911a4abfd42000ca407f
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2024-10-28 15:27:48 +0100

    lspkind

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -63,8 +63,8 @@ return { -- LSP Configuration & Plugins
           vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
-        -- map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        -- map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
         -- map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
         -- map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
         -- map('<leader>ds', require('telescope.builtin').lsp_document_sgymbols, '[D]ocument [S]ymbols')
