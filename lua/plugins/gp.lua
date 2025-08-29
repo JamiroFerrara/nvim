1eb6208c90100b3e69ce209a13c0910e828a8bc3
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2025-08-29 14:30:50 +0200

    updates

--- lua/plugins/gp.lua
+++ lua/plugins/gp.lua
@@ -1,17 +1,31 @@
 return {
   'robitx/gp.nvim',
   config = function()
-    -- TODO:
-    -- local conf = {
-    --   providers = {
-    --     openai = {
-    --       endpoint = 'https://api.openai.com/v1/chat/completions',
-    --       secret = os.getenv 'OPENAI_API_KEY',
-    --     },
-    --   },
-    -- }
-    -- require('gp').setup(conf)
-    --
-    -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+    require('gp').setup {
+      default_command_agent = 'ChatGPT4o',
+      default_chat_agent = 'ChatGPT4o',
+      providers = {
+        openai = {
+          endpoint = 'https://api.openai.com/v1/chat/completions',
+          secret = '',
+        },
+      },
+      agents = {
+        {
+          name = 'ExampleDisabledAgent',
+          disable = true,
+        },
+        {
+          name = 'ChatGPT4o',
+          chat = true,
+          command = false,
+          -- string with model name or table with model name and parameters
+          model = { model = 'gpt-4o', temperature = 1.1, top_p = 1 },
+          -- system prompt (use this to specify the persona/role of the AI)
+          -- system_prompt = require('gp.defaults').chat_system_prompt,
+          system_prompt = "You are a coding assistant. Help with writing, understanding, and refactoring code. Be concise, clear, and focus only on what's necessary. Provide relevant explanations or examples when needed. Optimize for readability and efficiency. Adapt based on user interactions to improve guidance continuously. Answer with the minimum amount of words possible.",
+        },
+      },
+    }
   end,
 }
