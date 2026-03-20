af7c5bc02b8a21acc26afbe6bc51925c5a56a6e7
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2026-03-21 00:48:07 +0100

    optimizations

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,8 @@
 -- See the kickstart.nvim README for more information
 return {
-  -- Colorscheme
-  { 'catppuccin/nvim', event = 'VeryLazy' },
-
   -- Improvements
   { 'lewis6991/impatient.nvim', config = function() require('impatient') end },
   { 'max397574/better-escape.nvim', event = 'InsertEnter'},
-  { 'ThePrimeagen/harpoon', event = 'VeryLazy', enabled = not _G.NVIM_TERMINAL_ONLY, },
   { 'haya14busa/is.vim', event = 'InsertEnter', enabled = not _G.NVIM_TERMINAL_ONLY, },
   { 'tpope/vim-sleuth', event = 'BufReadPre', enabled = not _G.NVIM_TERMINAL_ONLY, },
   { 'folke/zen-mode.nvim', event = 'VeryLazy', enabled = true,},
