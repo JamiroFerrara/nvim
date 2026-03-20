9912a40a12505e88d780e72d702671f69ad4b3d2
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2026-03-21 00:57:18 +0100

    org fix

--- lua/helpers/org.lua
+++ lua/helpers/org.lua
@@ -5,29 +5,30 @@ function M.smart_action()
   local line = vim.api.nvim_get_current_line()
 
   -- Fold if the line contains at least one '#'
-  if line:find("^*") then
-    vim.cmd("normal! za")
+  if line:find '^*' then
+    vim.cmd 'normal! za'
   end
 
   local link = require('orgmode.org.links.hyperlink').at_cursor()
   if link then
-    return require('orgmode').action('org_mappings.open_at_point');
+    return require('orgmode').action 'org_mappings.open_at_point'
   end
 
   -- Default to toggling checkboxes
-  return require('orgmode').action('org_mappings.toggle_checkbox');
+  return require('orgmode').action 'org_mappings.toggle_checkbox'
 end
 
 -- Fold expression for Orgmode headings
 function _G.org_foldexpr()
   local lnum = vim.v.lnum
   local line = vim.fn.getline(lnum)
-  local heading = line:match("^(%*+)%s")
+  local heading = line:match '^(%*+)%s'
   if heading then
     local level = #heading
-    return ">" .. level
+    local result = '>' .. level
+    return result
   end
-  return "="
+  return '='
 end
 
 M.set_org_folding = function()
@@ -38,24 +39,24 @@ M.set_org_folding = function()
   -- vim.api.nvim_set_keymap('n', '<leader>oa', "zR", { noremap = true, silent = true })
   -- vim.api.nvim_set_keymap('n', '<leader>oo', "zR", { noremap = true, silent = true })
 
-  vim.opt.foldtext = "" -- keep syntax highlighting
-  vim.opt_local.foldmethod = "expr"
-  vim.opt_local.foldexpr = "v:lua.org_foldexpr()"
+  vim.opt.foldtext = '' -- keep syntax highlighting
+  vim.opt_local.foldmethod = 'expr'
+  vim.opt_local.foldexpr = 'luaeval("org_foldexpr()")'
   vim.opt_local.foldlevel = 99
 end
 
 -- This toggles folds for all headings of a given level (like your Markdown version)
 M.fold_headings_of_level = function(level)
-  vim.cmd("keepjumps normal! gg")
-  local total_lines = vim.fn.line("$")
+  vim.cmd 'keepjumps normal! gg'
+  local total_lines = vim.fn.line '$'
   for line = 1, total_lines do
     local line_content = vim.fn.getline(line)
-    if line_content:match("^" .. string.rep("%*", level) .. "%s") then
-      vim.cmd(string.format("keepjumps call cursor(%d, 1)", line))
+    if line_content:match('^' .. string.rep('%*', level) .. '%s') then
+      vim.cmd(string.format('keepjumps call cursor(%d, 1)', line))
       local current_foldlevel = vim.fn.foldlevel(line)
       if current_foldlevel > 0 then
         if vim.fn.foldclosed(line) == -1 then
-          vim.cmd("normal! za")
+          vim.cmd 'normal! za'
         end
       end
     end
@@ -67,7 +68,7 @@ M.fold_org_headings = function(levels)
   for _, level in ipairs(levels) do
     M.fold_headings_of_level(level)
   end
-  vim.cmd("nohlsearch")
+  vim.cmd 'nohlsearch'
   vim.fn.winrestview(saved_view)
 end
 
