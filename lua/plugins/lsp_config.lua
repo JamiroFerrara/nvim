316318b6af318b8a8a842399290d3791e4abf077
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2026-03-21 00:27:50 +0100

    disable tstools because treesitter bug

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -37,25 +37,25 @@ return { -- LSP Configuration & Plugins
     --     require('java').setup()
     --   end,
     -- }, -- Setup nvim-java
-    {
-      'pmizio/typescript-tools.nvim',
-      dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-      opts = {},
-      config = function()
-        require('typescript-tools').setup {
-          on_attach = function(client, bufnr)
-            client.server_capabilities.documentFormattingProvider = false
-            client.server_capabilities.documentRangeFormattingProvider = false
-          end,
-          settings = {
-            jsx_close_tag = {
-              enable = true,
-              filetypes = { 'javascriptreact', 'typescriptreact' },
-            },
-          },
-        }
-      end,
-    },
+    -- {
+    --   'pmizio/typescript-tools.nvim',
+    --   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    --   opts = {},
+    --   config = function()
+    --     require('typescript-tools').setup {
+    --       on_attach = function(client, bufnr)
+    --         client.server_capabilities.documentFormattingProvider = false
+    --         client.server_capabilities.documentRangeFormattingProvider = false
+    --       end,
+    --       settings = {
+    --         jsx_close_tag = {
+    --           enable = true,
+    --           filetypes = { 'javascriptreact', 'typescriptreact' },
+    --         },
+    --       },
+    --     }
+    --   end,
+    -- },
   },
   config = function()
     -- Brief aside: **What is LSP?**
