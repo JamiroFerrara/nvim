4399e013b83a0ae235c8039fb8daf6866d04802b
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2026-03-21 01:26:31 +0100

    snacks restore

--- lua/terminal_plugins/telescope.lua
+++ lua/terminal_plugins/telescope.lua
@@ -17,8 +17,8 @@ function GET_IVY(opts)
     layout_config = {
       horizontal = {
         prompt_position = 'top',
-        preview_width = 1,
-        results_width = 1,
+        preview_width = 1.1,
+        results_width = 1.1,
       },
       vertical = {
         mirror = false,
@@ -188,7 +188,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = '[S]earch [F]iles' })
+    -- vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = '[S]earch [F]iles' })
     -- vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = '[S]earch [F]iles' })
     -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     -- vim.keymap.set('n', '<leader>fc', builtin.grep_string, { desc = '[S]earch current [W]ord' })
